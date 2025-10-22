a49e8cde46b1184ed20eb5cf4b7ecfaa7d22984e
Suraj Hattarakal <suriisuraj8gmail.com>
2025-10-23 01:05:17 +0530

    Added new course pages, updated video lesson and homepage with payment buttons

---
+++ courseData.js
@@ -0,0 +1,55 @@
+const courses = {
+  c: {
+    name: "C Programming",
+    lessons: [
+      { title: "Intro to C", video: "https://youtu.be/C_LESSON1" },
+      { title: "Pointers", video: "https://youtu.be/C_LESSON2" }
+    ],
+    pdf: "c-notes.pdf"
+  },
+  cpp: {
+    name: "C++ Programming",
+    lessons: [
+      { title: "Intro to C++", video: "https://youtu.be/CPP_LESSON1" },
+      { title: "OOP Basics", video: "https://youtu.be/CPP_LESSON2" }
+    ],
+    pdf: "cpp-notes.pdf"
+  },
+  java: {
+    name: "Java",
+    lessons: [
+      { title: "Intro to Java", video: "https://youtu.be/Java_LESSON1" },
+      { title: "OOP Concepts", video: "https://youtu.be/Java_LESSON2" }
+    ],
+    pdf: "java-notes.pdf"
+  },
+  python: {
+    name: "Python",
+    lessons: [
+      { title: "Intro to Python", video: "https://youtu.be/Python_LESSON1" },
+      { title: "Data Types", video: "https://youtu.be/Python_LESSON2" }
+    ],
+    pdf: "python-notes.pdf"
+  },
+  tally: {
+    name: "Tally",
+    lessons: [
+      { title: "Intro to Tally", video: "https://youtu.be/Tally_LESSON1" }
+    ],
+    pdf: "tally-notes.pdf"
+  },
+  bcs: {
+    name: "BCS",
+    lessons: [
+      { title: "Intro to BCS", video: "https://youtu.be/BCS_LESSON1" }
+    ],
+    pdf: "bcs-notes.pdf"
+  },
+  pgdca: {
+    name: "PGDCA",
+    lessons: [
+      { title: "Intro to PGDCA", video: "https://youtu.be/PGDCA_LESSON1" }
+    ],
+    pdf: "pgdca-notes.pdf"
+  }
+};
